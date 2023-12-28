d179a881c00eb50e2a3050d5ebe109480a4619d0
Jedidiah Amaraegbu <imunacode@gmail.com>
2023-12-28 23:14:24 +0100

    feat: shadcn/ui setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from 'next'
 import { Manrope } from "next/font/google";
-import "@/app/globals.css";
+import "@/styles/globals.css";
+import Nav from "@/components/nav";
+import Footer from "@/components/footer";
+import { PropsWithChildren } from "react";
+import { cn } from "@/lib/utils";
 
 const manrope = Manrope({ subsets: ["latin"], variable: "--font-manrope" });
 
@@ -22,14 +26,19 @@ export const metadata: Metadata = {
   description: "Welcome to Free Tribe Network",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html lang="en">
-      <body className={`${manrope.variable} font-manrope`}>{children}</body>
+    <html lang="en" className="h-full">
+      <body
+        className={cn(
+          "font-manrope min-h-full grid grid-rows-[auto,1fr,auto]",
+          manrope.variable
+        )}
+      >
+        <Nav />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
