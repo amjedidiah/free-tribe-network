334935d1120dd2df839b35d8b809100dfe4108d8
Jedidiah Amaraegbu <imunacode@gmail.com>
2024-03-08 22:40:01 +0100

    refactor: code cleanup and refactoring

--- src/app/news-events/page.tsx
+++ src/app/news-events/page.tsx
@@ -1,5 +1,6 @@
 import ActivityTabs from "@/components/news-events/activity-tabs";
 import Banner from "@/components/shared/banner";
+import { defaultNewsEventSection } from "@/lib/data";
 import { Fragment } from "react";
 
 export default function NewsEvents() {
@@ -8,7 +9,10 @@ export default function NewsEvents() {
       <Banner imageTitle="news-banner" title="News and Events" />
       <section className="py-10 lg:py-14">
         <article className="container">
-          <ActivityTabs />
+          <ActivityTabs
+            initUrlSection={defaultNewsEventSection}
+            shouldScroll={false}
+          />
         </article>
       </section>
     </Fragment>
