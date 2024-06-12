d089ac1848354fab991f2ad0a27634aee35b9f10
Jedidiah Amaraegbu <imunacode@gmail.com>
2024-06-12 11:20:56 +0100

    fix(app): resources minimised footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,59 +1,71 @@
+"use client";
 import ContactCard from "@/components/layout/contact-card";
 import SubscribeFormContainer from "@/components/subscribe-form-container";
-import { Link } from "@/lib/i18n.config";
+import { Link, usePathname } from "@/lib/i18n.config";
 import { contactChannels, currentYear, routes } from "@/lib/data";
 import Socials from "@/components/shared/socials";
-import ContentImage from "@/components/shared/content-image";
+import ContentImageClient from "@/components/shared/content-image-client";
 import { useTranslations } from "next-intl";
+import ShouldRender from "@/components/shared/should-render";
 
-export default function Footer() {
+export default function Footer({ minimal = false }) {
+  const pathname = usePathname();
   const t = useTranslations("Layout");
+  minimal = pathname === "/resources";
 
   return (
     <footer>
-      <section className="container" id="contact-us">
-        <article className="py-12 lg:py-24 h3-gap">
-          <h3 className=" text-black">{t('Heading.title')}</h3>
-          <p>{t('Heading.description')}</p>
-        </article>
+      <ShouldRender condition={!minimal}>
+        <section className="container" id="contact-us">
+          <article className="py-12 lg:py-24 h3-gap">
+            <h3 className=" text-black">{t("Heading.title")}</h3>
+            <p>{t("Heading.description")}</p>
+          </article>
 
-        <article className="pb-12 lg:pb-24 grid lg:grid-cols-3 gap-6">
-          {contactChannels.map((i) => (
-            <ContactCard key={i.title} {...i} />
-          ))}
-        </article>
-      </section>
+          <article className="pb-12 lg:pb-24 grid lg:grid-cols-3 gap-6">
+            {contactChannels.map((i) => (
+              <ContactCard key={i.title} {...i} />
+            ))}
+          </article>
+        </section>
+      </ShouldRender>
       <section className="bg-secondary-900 py-10">
         <div className="container grid gap-6 lg:gap-8 text-white">
-          <div className="flex flex-col lg:flex-row justify-between gap-10">
-            <div className="flex flex-col gap-6">
-              <Link href="/">
-                <div className="relative h-[60px] w-[60px]">
-                  <ContentImage
-                    title="free-tribe-white"
-                    fill
-                    sizes="100%"
-                    style={{ objectFit: "cover", objectPosition: "center" }}
-                  />
+          <ShouldRender condition={!minimal}>
+            <div className="flex flex-col lg:flex-row justify-between gap-10">
+              <div className="flex flex-col gap-6">
+                <Link href="/">
+                  <div className="relative h-[60px] w-[60px]">
+                    <ContentImageClient
+                      title="free-tribe-white"
+                      fill
+                      sizes="100%"
+                      style={{ objectFit: "cover", objectPosition: "center" }}
+                    />
+                  </div>
+                </Link>
+                <div className="grid gap-6">
+                  <ul className="flex flex-col lg:flex-row gap-4">
+                    {routes.map(({ links, ...item }) => (
+                      <li key={item.label}>
+                        <Link href={item.href}>
+                          {t(`Contact.${item.label}` as any)}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                  <Socials className="[&_*]:text-white" />
                 </div>
-              </Link>
-              <div className="grid gap-6">
-                <ul className="flex flex-col lg:flex-row gap-4">
-                  {routes.map(({ links, ...item }) => (
-                    <li key={item.label}>
-                      <Link href={item.href}>{t(`Contact.${item.label}` as any)}</Link>
-                    </li>
-                  ))}
-                </ul>
-                <Socials className="[&_*]:text-white" />
               </div>
-            </div>
 
-            <SubscribeFormContainer />
-          </div>
-          <hr />
+              <SubscribeFormContainer />
+            </div>
+            <hr />
+          </ShouldRender>
           <div className="flex flex-col lg:flex-row items-center justify-between gap-1">
-            <p>&copy; {currentYear} {t("Footer.Copyright")}</p>
+            <p>
+              &copy; {currentYear} {t("Footer.Copyright")}
+            </p>
             <p>{t("Footer.Rights")}</p>
           </div>
         </div>
