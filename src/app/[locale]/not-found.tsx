774acdf999458ae8df9b7bf7285543c50538a115
Jedidiah Amaraegbu <imunacode@gmail.com>
2024-07-03 11:01:22 +0100

    fix(app): 404 page
    - 404 page for next-intl using [guide](https://next-intl-docs.vercel.app/docs/environments/error-files)

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,19 +1,22 @@
 import { Link } from "@/lib/i18n.config";
+import { useTranslations } from "next-intl";
 
 export default function NotFound() {
+  const t = useTranslations("NotFound");
+
   return (
     <section className="mt-36 py-10 lg:py-14">
       <article className="container text-center">
         <h1 className="font-extralight text-8xl sm:text-[10rem] md:text-[12rem] lg:text-[14rem]">
-          404
+          {t("title")}
         </h1>
         <div className="relative flex flex-col gap-2 items-center">
-          <p>We&apos;re sorry, this page does not exist</p>
+          <p>{t("text")}</p>
           <Link
             href="/"
             className="bg-primary-500 text-white px-5 py-3 rounded shadow-md"
           >
-            Return home
+            {t("action")}
           </Link>
         </div>
       </article>
